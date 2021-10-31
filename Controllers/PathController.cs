879b88465cecb677a579337cead05beb82041cc5
MikkiSeidenschnur <mikki.seidenschnur@gmail.com>
2021-10-31 10:38:49 +0100

    fix file to be read from memory instead of body

--- Controllers/PathController.cs
+++ Controllers/PathController.cs
@@ -14,33 +14,39 @@ namespace PollutionSolution.Controllers
     public class PathController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get([FromQuery] int start, [FromQuery] int finish, [FromBody] List<Models.Node> items)
+        public IActionResult Get([FromQuery] int start, [FromQuery] int finish)
         {
             var maxAgentSpeed = Velocity.FromKilometersPerHour(140);
-
-            // Create directed graph with node a and b, and a one-way direction from a to b
-            var nodes = new List<Node>();
-
-            foreach (var item in items)
+            using (StreamReader r = new StreamReader("C:/Users/msei/source/repos/Hackathon2021/Resources/NodeList.json"))
             {
-                var node = new Node(new Position(item.X, item.Y));
-                nodes.Add(node);
-            }
-            for (var i = 0; i < nodes.Count; i++)
-            {
-                var children = items[i].Children;
-                var node = nodes[i];
-                foreach (var child in children)
+                string json = r.ReadToEnd();
+                List<Models.Node> items = JsonConvert.DeserializeObject<List<Models.Node>>(json);
+                // Create directed graph with node a and b, and a one-way direction from a to b
+                var nodes = new List<Node>();
+
+                foreach (var item in items)
                 {
-                    var nodeB = nodes[child - 1];
-                    node.Connect(nodeB, Velocity.FromKilometersPerHour(items[child-1].Pollution));
+                    var node = new Node(new Position(item.X, item.Y));
+                    nodes.Add(node);
                 }
+                for (var i = 0; i < nodes.Count; i++)
+                {
+                    var children = items[i].Children;
+                    var node = nodes[i];
+                    foreach (var child in children)
+                    {
+                        var nodeB = nodes[child - 1];
+                        node.Connect(nodeB, Velocity.FromKilometersPerHour(items[child - 1].Pollution));
+                    }
+                }
+
+
+                var pathFinder = new PathFinder();
+                var path = pathFinder.FindPath(nodes[start], nodes[finish], maximumVelocity: maxAgentSpeed);
+
+
+                return Ok(path);
             }
-
-            var pathFinder = new PathFinder();
-            var path = pathFinder.FindPath(nodes[start], nodes[finish], maximumVelocity: maxAgentSpeed);
-
-            return Ok(path);
         }
     }
 }
