9485fe2724fe06caf2ad7fddbcdfe48df0b13feb
ALharchi <alha@kglakademi.dk>
2021-10-31 10:22:53 +0100

    initial upload

---
+++ Controllers/PathController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Roy_T.AStar.Graphs;
+using Roy_T.AStar.Primitives;
+using Roy_T.AStar.Paths;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PollutionSolution.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PathController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get([FromQuery] int start, [FromQuery] int finish, [FromBody] List<Models.Node> items)
+        {
+            var maxAgentSpeed = Velocity.FromKilometersPerHour(140);
+
+            // Create directed graph with node a and b, and a one-way direction from a to b
+            var nodes = new List<Node>();
+
+            foreach (var item in items)
+            {
+                var node = new Node(new Position(item.X, item.Y));
+                nodes.Add(node);
+            }
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var children = items[i].Children;
+                var node = nodes[i];
+                foreach (var child in children)
+                {
+                    var nodeB = nodes[child - 1];
+                    node.Connect(nodeB, Velocity.FromKilometersPerHour(items[child-1].Pollution));
+                }
+            }
+
+            var pathFinder = new PathFinder();
+            var path = pathFinder.FindPath(nodes[start], nodes[finish], maximumVelocity: maxAgentSpeed);
+
+            return Ok(path);
+        }
+    }
+}
---
+++ Models/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PollutionSolution.Models
+{
+    public class Node
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Pollution { get; set; }
+        public IEnumerable<int> Children { get; set; }
+    }
+}
